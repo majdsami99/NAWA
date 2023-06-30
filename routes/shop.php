1eaf4c46522825641eb65c954c1e82c52d978af2
majd sami <majdsami150@gmail.com>
2023-06-30 23:35:32 +0300

    Shop Page: Allow users to add Reviews to products and display the reviews on the product page.

---
+++ routes/shop.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route ;
+
+Route::get('/products/{product}',[ProductController::class,'show'])
+->name('shop.products.show');
+
--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,5 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php'; //ملف مليان راتس
 require __DIR__.'/admin.php';
-//require __DIR__.'/shop.php';
+require __DIR__.'/shop.php';
 
